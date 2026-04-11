6966b4ea95de9d4abc5e3f992996fea96fceaea7
KamingLo <lokaming86@gmail.com>
2026-04-11 13:00:15 +0700

    new files and features

--- python/.py
+++ python/.py
@@ -1,1 +1,9 @@
-!pwd
+import torch
+
+print(f"Versi PyTorch: {torch.__version__}")
+if torch.cuda.is_available():
+    device = 0 # Menggunakan GPU pertama
+    print(f"GPU Terdeteksi: {torch.cuda.get_device_name(0)}")
+else:
+    device = 'cpu'
+    print("GPU TIDAK terdeteksi, menggunakan CPU.")
